de588f7ac4feee286d5573e36bb74cdd454060dd
Kevin Xu <kevinpxu21@gmail.com>
2023-01-25 19:02:52 -0600

    added a font to the page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Nav from './Components/Nav';
 import About from './Components/About';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['Open Sans', 'Proza Libre'].join(','),
+  },
+});
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Nav />
       <About />
-    </>
+    </ThemeProvider>
   );
 }
 
