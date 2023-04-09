75f03b9b7674c043fae7469b2539500af9513918
KevinPXu <kevinpxu21@gmail.com>
2023-04-09 18:29:05 -0400

    added a parallax background to the landing page

--- src/Pages/LandingPage.js
+++ src/Pages/LandingPage.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 
 function LandingPage() {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset;
+      const parallax = scrollTop * 0.5;
+      container.style.backgroundPositionY = `${parallax}px`;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div
-      style={{
-        height: '100vh',
-        backgroundColor: '#000000',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <h1 className='landingHeader'>WELCOME</h1>
+      className='LandingContainer'
+      ref={containerRef}>
+      <div className='landingHeader'>
+        <h1 className='text'>WELCOME</h1>
+      </div>
     </div>
   );
 }
