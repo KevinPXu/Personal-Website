4d3abc4d1dee15981a85c177c0a1d37ecf3fab64
KevinPXu <kevinpxu21@gmail.com>
2023-04-10 12:14:11 -0400

    updated style to about page

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,4 +1,4 @@
-import { Avatar, Divider, Grid, Typography } from '@mui/material';
+import { Avatar } from '@mui/material';
 import ProfilePic from '../../src/Assets/images/ProfilePic.jpg';
 import React from 'react';
 
@@ -8,41 +8,20 @@ function About() {
       style={{ height: '100vh' }}
       id='About'
       className='container'>
-      <Divider
-        color='#9E6328'
-        variant='inset'
-        //style={{ background: '#9E6328' }}
-        textAlign='left'>
-        About Me
-      </Divider>
-      <Grid
-        container
-        my={5}
-        spacing={1}>
-        <Grid
-          item
-          xs={8}
-          display='flex'
-          justifyContent='center'
-          alignItems='center'>
-          <Typography
-            mx={8}
-            variant='p'
-            component='p'>
-            Hi! My name is Kevin Xu, I graduated from University of Miami with a
-            Bachelors in Health Science. I deeply care about helping others and
-            I have decided to pursue that through the use of technology. My
-            interest was first piqued
-          </Typography>
-        </Grid>
-        <Grid
-          item
-          xs={4}>
-          <Avatar
-            sx={{ width: 250, height: 250 }}
-            src={ProfilePic}></Avatar>
-        </Grid>
-      </Grid>
+      <div className='aboutHeaderContainer'>
+        <h1 className='aboutHeader'>About Me</h1>
+      </div>
+      <p className='aboutText'>
+        Hi! My name is Kevin Xu, I graduated from University of Miami with a
+        Bachelors in Health Science. I deeply care about helping others and I
+        have decided to pursue that through the use of technology. My interest
+        was first piqued
+      </p>
+
+      <Avatar
+        sx={{ width: 250, height: 250 }}
+        style={{ position: 'absolute', top: '130%', right: '10%' }}
+        src={ProfilePic}></Avatar>
     </div>
   );
 }
